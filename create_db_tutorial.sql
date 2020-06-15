1f8974b8cfea6a8ddf8f472d447d983ba850af3e
Watson Carawon <wcarawon@outlook.com>
2020-06-15 15:20:02 -0400

    Correct db_tutorial and table_tutorial

--- create_db_tutorial.sql
+++ create_db_tutorial.sql
@@ -1,15 +1,9 @@
-USE [testDB]
--- Create a new table called 'Customers' in schema 'dbo'
--- Drop the table if it already exists
-IF OBJECT_ID('dbo.customers', 'U') IS NOT NULL
-DROP TABLE dbo.Customers
+USE master
 GO
---Create the table in the specified schema
-CREATE TABLE dbo.Customers
-(
-	CustomerID	INT	NOT NULL	PRIMARY KEY,
-	Name		[NVARCHAR](50)	NOT NULL,
-	Location	[NVARCHAR](50)	NOT NULL,
-	Email		[NVARCHAR](50)	NOT NULL,
-);
+IF NOT EXISTS (
+   SELECT name
+   FROM sys.databases
+   WHERE name = N'testDB'
+)
+CREATE DATABASE [testDB]
 GO
